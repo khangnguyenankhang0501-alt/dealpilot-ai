7e40c18ea88a02d00c22cf4ad222ca82d1a1cbe7
khangnguyenankhang0501-alt <khang.nguyenankhang0501@gmail.com>
2026-09-01 23:14:22 +0700

    Fix homepage cache

--- app/page.tsx
+++ app/page.tsx
@@ -19,14 +19,14 @@ export default async function HomePage() {
     .from("coupons")
     .select(
       `
-  *,
-  stores!store_id (
-    id,
-    name,
-    slug,
-    logo_url
-  )
-`,
+      *,
+      stores!store_id (
+        id,
+        name,
+        slug,
+        logo_url
+      )
+    `,
     )
     .eq("status", "Active")
     .or(`expires_at.is.null,expires_at.gte.${today}`)
